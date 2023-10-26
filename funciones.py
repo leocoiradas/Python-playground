a576a67f81b368bb491211945eb16742aa31bcf9
leocoiradas <coiradasalan@gmail.com>
2023-10-26 19:16:40 -0300

    Ejercicio 1
    Función de Saludo: Crea una función llamada saludar que acepte un nombre como argumento y muestre un mensaje de saludo, como "¡Hola, [nombre]!".

---
+++ funciones.py
@@ -0,0 +1,7 @@
+#Functions
+
+##Crear una función que acepte un nombre como parametro e imprima un saludo
+def greeting(name):
+    print(f"Hi {name} I hope that you're having an amazing day!!")
+
+greeting("Alan")
