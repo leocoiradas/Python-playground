97031257e39cf9b059acb4efa1e49738299f8125
leocoiradas <coiradasalan@gmail.com>
2023-10-26 19:25:16 -0300

    Ejercicio 2
    Función para Calcular el Área de un Círculo: Crea una función llamada area_circulo que tome el radio como argumento y calcule el área de un círculo utilizando la fórmula π * r^2.

--- funciones.py
+++ funciones.py
@@ -1,7 +1,20 @@
+import math
 #Functions
 
 ##Crear una función que acepte un nombre como parametro e imprima un saludo
-def greeting(name):
+'''def greeting(name):
     print(f"Hi {name} I hope that you're having an amazing day!!")
 
-greeting("Alan")
+greeting("Alan")'''
+
+##Función para calcular el área de un círculo. toma radio como argumento y calcula el área con la fórmula pi * radio^2
+
+def area(radius):
+    pi = math.pi
+    circleArea =  pi * math.pow(radius, 2)
+    '''math.pow es un metodo de la librería math que se usa para calcular potencias, recibe 2 parámetros:
+    el primero es la base y el segundo el exponente al que se va a elevar la base para calcular la potencia'''
+    
+    print(circleArea)
+
+area(34)
