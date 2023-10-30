0e0c87cec8ec00259b38acc0a360b19bef29c5e1
leocoiradas <coiradasalan@gmail.com>
2023-10-30 13:11:54 -0300

    Ejercicio 2: Clase Coche
    Clase Coche: Diseña una clase Coche con atributos como marca, modelo y año. Crea varios objetos de coches y muestra sus detalles.

--- orientacionAObjetos.py
+++ orientacionAObjetos.py
@@ -3,7 +3,7 @@
 
 ##Clase persona: Crear una clase persona con atributos como nombre edad y género. Crear varios objetos de esta clase
 
-class Persona:
+'''class Persona:
     def __init__(self, nombre):
         self.nombre = nombre
         self.edad_valor = None
@@ -22,4 +22,24 @@ personaOne.genero("Masculino")
 
 personaTwo = Persona("Lucila")
 personaTwo.edad(32)
-personaTwo.genero("Femenino")
+personaTwo.genero("Femenino")'''
+
+##Clase coche: Crear clase coche con atributos y metodos como marca modelo y año. Crear objetos instanciando dicha clase
+
+class Coche:
+    def __init__(self, marca, modelo, anio):
+        self.marca = marca
+        self.modelo = modelo
+        self.anio = anio
+    def coche_marca(self):
+        print(f"La marca del auto es {self.marca}")
+    def coche_modelo(self):
+        print(f"El modelo del auto es {self.modelo}")
+    def coche_anio(self):
+        print(f"El año de este coche es {self.anio}")
+        
+ferrari = Coche("Ferrari", "Deportivo", "2023")
+ferrari.coche_anio()
+ferrari.coche_modelo()
+ferrari.coche_marca()
+    
